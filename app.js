944c616071a3732420c7de9eb62491e53683432a
ronaldjeq9512 <ronaldjhairestelaquintana@gmail.com>
2024-05-08 22:14:19 -0500

    added first commits

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hola mundo")
+
+// Tipos de variables
+
+let variable1 = 'hola' // let es un tipo de valor que puede ser reasignado, puede declararse también sin necesidad de asignarle un valor, por lo general se infiere el tipo de variable
+
+let variable2; // aquí vemos un ejemplo en el cual se declara sin necesidad de asignarle un valor
+variable2 = 'hola2' // luego podemos asignarle un valor 
+variable2= 3 // también puede asignarse otro valor y de otro tipo, en este caso un number
+
+const variable3 = 'holas' // const es un tipo de valor que no puede reasignarse, se mantiene constante
---
+++ readme.md
@@ -0,0 +1,3 @@
+# JAVASCRIPT
+
+Es un lenguaje de alto nivel, utilizado junto con html y css, originalmente se desarrolló para la creación de scripts en páginas web, pero con el pasar de los tiempos ha evolucionado permitiendo no solo el desarrollo de aplicaciones web, sino móviles, juegos, aplicaciones de escritorio y móviles
